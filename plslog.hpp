ef44ef09475a8a174e2993d7ca54cc8429b784e0
Skooogi <kassuskog@gmail.com>
2021-11-01 20:50:46 +0200

    cleaned up a bit

--- plslog.hpp
+++ plslog.hpp
@@ -5,37 +5,40 @@
 /*	Usage:
  *	First declare the message severity level
  *	(can be changed later)
- *	plslog::SEVERITY plslog::level = plslog::SEVERITY::INFO;
+ *	plslog::levels plslog::level = plslog::levels::INFO;
  *
- *	plslog::comment/info/data/warning/error("Your string")("on")("separate")("lines");
- *	plslog::info("Your string") << "This" << "is" << "the" << "next" << "line\n"
- *	plslog::error("Something is wrong") << 0 << 123.44f << '\n'
+ *	plslog::comment/info/data/warning/error("Your string")("with")("spaces")(0.3f);
+ *	plslog::info("Your string") << "without" << "spaces" << 0.3f << "\n"
  */
 class plslog {
 
-	enum class SEVERITY { COMMENT, INFO, DATA, WARNING, ERROR, SILENT};
 	public:
-		static plslog comment(const std::string& str, std::ostream& os = std::cout) { return  plslog(SEVERITY::COMMENT, os)(str); }
-		static plslog info(const std::string& str, std::ostream& os = std::cout) { return  plslog(SEVERITY::INFO, os)(str); }
-		static plslog data(const std::string& str, std::ostream& os = std::cout) { return  plslog(SEVERITY::DATA, os)(str); }
-		static plslog warning(const std::string& str, std::ostream& os = std::cout) { return  plslog(SEVERITY::WARNING, os)(str); }
-		static plslog error(const std::string& str, std::ostream& os = std::cout) { return  plslog(SEVERITY::ERROR, os)(str); }
+		enum class levels { COMMENT, INFO, DATA, WARNING, ERROR, SILENT};
+		static levels global_level;
+
+		static plslog comment(const std::string& str) { return plslog(levels::COMMENT)(str); }
+		static plslog info(const std::string& str) { return plslog(levels::INFO)(str); }
+		static plslog data(const std::string& str) { return plslog(levels::DATA)(str); }
+		static plslog warning(const std::string& str) { return plslog(levels::WARNING)(str); }
+		static plslog error(const std::string& str) { return plslog(levels::ERROR)(str); }
+
+		static void set_level(levels new_level) { global_level = new_level; }
 
 		template <typename T>
 		const plslog& operator()(T& printable) const {
 
-			if(message_level_ == plslog::SEVERITY::COMMENT)
-				os_ << printable << '\n';
+			if(message_level_ == plslog::levels::COMMENT)
+				std::cout << printable << ' ';
 
-			if(message_level_ >= level) {
-				if(message_level_ == plslog::SEVERITY::INFO)
-					os_ << printable << "\033[0m\n";
-				if(message_level_ == plslog::SEVERITY::DATA)
-					os_ << "\033[1;32m" << printable << "\033[0m\n";
-				if(message_level_ == plslog::SEVERITY::WARNING)
-					os_ << "\033[1;33m" << printable << "\033[0m\n";
-				if(message_level_ == plslog::SEVERITY::ERROR)
-					os_ << "\033[1;31m" << printable << "\033[0m\n";
+			if(message_level_ >= global_level) {
+				if(message_level_ == plslog::levels::INFO)
+					std::cout << printable << "\033[0m ";
+				if(message_level_ == plslog::levels::DATA)
+					std::cout << "\033[1;32m" << printable << "\033[0m ";
+				if(message_level_ == plslog::levels::WARNING)
+					std::cout << "\033[1;33m" << printable << "\033[0m ";
+				if(message_level_ == plslog::levels::ERROR)
+					std::cout << "\033[1;31m" << printable << "\033[0m ";
 			}
 				
 			return *this;
@@ -44,41 +47,37 @@ class plslog {
 		template <typename T>
 		const plslog& operator<<(T& printable) const {
 
-			if(message_level_ == plslog::SEVERITY::COMMENT)
-				os_ << printable;
+			if(message_level_ == plslog::levels::COMMENT)
+				std::cout << printable;
 
-			if(message_level_ >= level) {
-				if(message_level_ == plslog::SEVERITY::INFO)
-					os_ << printable << "\033[0m";
-				if(message_level_ == plslog::SEVERITY::DATA)
-					os_ << "\033[1;32m" << printable << "\033[0m";
-				if(message_level_ == plslog::SEVERITY::WARNING)
-					os_ << "\033[1;33m" << printable << "\033[0m";
-				if(message_level_ == plslog::SEVERITY::ERROR)
-					os_ << "\033[1;31m" << printable << "\033[0m";
+			if(message_level_ >= global_level) {
+				if(message_level_ == plslog::levels::INFO)
+					std::cout << printable << "\033[0m";
+				if(message_level_ == plslog::levels::DATA)
+					std::cout << "\033[1;32m" << printable << "\033[0m";
+				if(message_level_ == plslog::levels::WARNING)
+					std::cout << "\033[1;33m" << printable << "\033[0m";
+				if(message_level_ == plslog::levels::ERROR)
+					std::cout << "\033[1;31m" << printable << "\033[0m";
 			}
 				
 			return *this;
 		}
 
 	private:
-		plslog(SEVERITY message_level, std::ostream& os) : message_level_(message_level), os_(os) {
+		plslog(levels message_level = levels::COMMENT) : message_level_(message_level) {
 			
-			if(message_level_ >= level) {
-				if(message_level_ == plslog::SEVERITY::INFO)
-					os_ <<  "INFO: ";
-				if(message_level_ == plslog::SEVERITY::DATA)
-					os_ << "\033[1;32m" << "DATA: ";
-				if(message_level_ == plslog::SEVERITY::WARNING)
-					os_ << "\033[1;33m" << "WARNING: ";
-				if(message_level_ == plslog::SEVERITY::ERROR)
-					os_ << "\033[1;4;31m" << "ERROR:\033[0m ";
+			if(message_level_ >= global_level) {
+				if(message_level_ == plslog::levels::INFO)
+					std::cout <<  "INFO: ";
+				if(message_level_ == plslog::levels::DATA)
+					std::cout << "\033[1;32m" << "DATA: ";
+				if(message_level_ == plslog::levels::WARNING)
+					std::cout << "\033[1;33m" << "WARNING: ";
+				if(message_level_ == plslog::levels::ERROR)
+					std::cout << "\033[1;4;31m" << "ERROR:\033[0m ";
 			}
 		}
-
 	private:
-		std::ostream& os_;
-		plslog::SEVERITY message_level_;
-		static SEVERITY level;
-
+		levels message_level_;
 };
